a9ffd563d541ed7e3ad51c427a327a72b6a83876
esdrasdutra <esdrasfdutra@gmail.com>
2021-05-28 14:11:06 -0300

    First Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { DashboardPageComponent } from './dashboard/dashboard-page/dashboard-page.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
