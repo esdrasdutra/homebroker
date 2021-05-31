4cb3d4cbdfb254875553253f9f93112f3a90c1d9
esdrasdutra <esdrasfdutra@gmail.com>
2021-05-31 13:30:27 -0300

    Frontend Developed

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { StockCardComponent } from './stock-card/stock-card.component';
 
 
 
 @NgModule({
   declarations: [
-    DashboardPageComponent
+    DashboardPageComponent,
+    StockCardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
   ],
+  exports: [
+    DashboardPageComponent,
+  ]
 })
 export class DashboardModule { }
